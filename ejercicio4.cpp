c1d5aebe37a7a806a2afefebf24c3b9abc0b45e7
csantillgar <csantill@myuax.com>
2023-11-04 14:29:08 +0100

    Ejercicio4
    ejercicio4.cintia

---
+++ ejercicio4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<exception>
+#include <string>
+using namespace std;
+
+class MiExcepcion: public exception{
+private:
+    string mensaje;
+public:
+    explicit MiExcepcion(const string& msg): mensaje(msg){}
+    const char* what() const noexcept override {
+        return mensaje.c_str();
+    }
+};
+void lanzaExcepcion(){
+    throw MiExcepcion("Ha ocurrido un error en la funcion lanzaExcepcion");
+}
+int main(){
+try{
+    lanzaExcepcion();
+}catch(const MiExcepcion& e){
+    cout<<"Excepcion capturada: "<<e.what()<<endl;
+}
+    return 0;
+}
